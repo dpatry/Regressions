ab1d123f8eab6555ce687a67e381d8af040dc6b5
ÃÃcovijt <denispatry@outlook.com>
2020-06-04 17:51:08 -0400

    Changes

--- Correlation.R
+++ Correlation.R
@@ -2,7 +2,7 @@
 library(tidyverse)
 library(HistData)
 data("GaltonFamilies")
-
+View
 set.seed(1983)
 galton_heights <- GaltonFamilies %>%
   filter(gender == "male") %>%
@@ -22,5 +22,31 @@ galton_heights %>%
   geom_point(alpha = 0.5)
 
 # Correlation
+
+# rho <- mean(scale(x), scale(y))
 galton_heights %>% summarise(r = cor(father, son)) %>% pull(r)
+
+
+### Sample correlation is a random variable
+R <- sample_n(galton_heights, 25, replace = TRUE) %>%
+  summarize(r = cor(father, son))
+R
+
+B <- 1000
+N <- 25
+R <- replicate(B, {
+  sample_n(galton_heights, N, replace = TRUE) %>%
+    summarize(r = cor(father, son)) %>%
+    pull(r)
+})
+
+qplot(R, geom = "histogram", binwidth = 0.05, color = I("black"))
+mean(R)
+sd(R)
+
+data.frame(R) %>% 
+  ggplot(aes(sample = R)) +
+  stat_qq() +
+  geom_abline(intercept = mean(R), slope = sqrt((1 -mean(R)^2)/(N-2)))
+
 
